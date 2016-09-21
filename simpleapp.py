81bcd04ed10375e00665c279268be6d1a8867827
chenx <bufubaoni@163.com>
2016-09-21 23:04:24 +0800

    begin add the code

---
+++ simpleapp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# just is a simple app
+from bottle import route, run
+
+
+@route('/')
+def index():
+    return "this is index for roote"
+
+@route("/userlist")
+def userlist():
+    return "this is user list"
+
+@route("/adduser")
+def adduser():
+    return "this is add user"
+
+@route("/userinfo")
+@route("/userinfo/<userid:int>")
+def userinf(userid):
+    if userid:
+        return "user id is {id}".format(id=userid)
+    else:
+        return "user info"
+
+run(host='localhost', port=8000, debug=True)
