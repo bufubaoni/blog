0f53ee83b228c7ebf794958a127c2bf3e617226c
chenx <bufubaoni@163.com>
2016-09-25 21:14:16 +0800

    creat config.py

--- setup.py
+++ setup.py
@@ -11,3 +11,14 @@ setup(
         "beaker"
     ],
 )
+
+with open("config.py", "w") as f:
+    f.writelines("""dbconfig = 'mysql://username:password@host/databasename'
+    session_option = {
+        "session.type": "file",
+        # "session.cookie_expires": 30,
+        "session.data_dir": "./session",
+        "session.auto": True,
+        "Session.save_accessed_time": True,
+        "Session.timeout": 30
+    }""")
